97a9bb8c11237580c8114025b3b0238e10c21bd2
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2023-03-15 06:54:50 +0000

    Add debug trace

--- api/chatgpt/index.js
+++ api/chatgpt/index.js
@@ -18,9 +18,13 @@ module.exports = async function (context, req) {
     // context.res.json({
     //     text: "Hello from the API"
     // });
+    context.log("Chat");
+    context.log(openaiurl);
+    context.log(openaipikey);
 
     const m = JSON.parse(req.body);
     context.log(m);
+
     const header = req.headers['x-ms-client-principal'];
     const encoded = Buffer.from(header, 'base64');
     const decoded = encoded.toString('ascii');
