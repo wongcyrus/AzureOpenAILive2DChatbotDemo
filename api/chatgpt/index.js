c95991d89b821c9d2d8233b295d46c4f5a0032e2
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2023-03-15 06:10:51 +0000

    Add chatgpt proxy.

--- api/chatgpt/index.js
+++ api/chatgpt/index.js
@@ -1,3 +1,8 @@
+
+const openaiurl = "OPENAIURL";
+const openaipikey = "OPENAIPIKEY";
+
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
@@ -10,7 +15,7 @@ module.exports = async function (context, req) {
     //     // status: 200, /* Defaults to 200 */
     //     body: responseMessage
     // };
-    
+
     // context.res.json({
     //     text: "Hello from the API"
     // });
@@ -18,10 +23,23 @@ module.exports = async function (context, req) {
     const header = req.headers['x-ms-client-principal'];
     const encoded = Buffer.from(header, 'base64');
     const decoded = encoded.toString('ascii');
-  
-    context.res = {
-      body: {
-        clientPrincipal: JSON.parse(decoded),
-      },
-    };
+
+    const repsonse = await fetch(openaiurl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'api-key': openaipikey,
+        },
+        body: JSON.stringify(m)
+    });
+    const json = await repsonse.json();
+
+    context.res.json(json);
+
+
+    // context.res = {
+    //     body: {
+    //         clientPrincipal: JSON.parse(decoded),
+    //     },
+    // };
 }
