e19a11ead09cab511eb14ae57d27362523bdabb0
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2023-03-16 03:13:48 +0000

    fix

--- api/text-to-speech/index.js
+++ api/text-to-speech/index.js
@@ -9,14 +9,14 @@ module.exports = async function (context, req) {
     // const header = req.headers['x-ms-client-principal'];
     // const encoded = Buffer.from(header, 'base64');
     // const decoded = encoded.toString('ascii');
-    body = body.slice(1, -1).split(`\\n`).join('');
+    // body = body.slice(1, -1).split(`\\n`).join('');
     try {
         const headers = {
             'Content-Type': 'application/ssml+xml',
             'X-Microsoft-OutputFormat': 'riff-8khz-16bit-mono-pcm',
             'Ocp-Apim-Subscription-Key': ttsapikey,
         }
-        const res = await axios.post(`https://${ttsregion}.tts.speech.microsoft.com/cognitiveservices/v1`, JSON.stringify(body), {
+        const res = await axios.post(`https://${ttsregion}.tts.speech.microsoft.com/cognitiveservices/v1`, body, {
             headers: headers
         });
         context.res = {
