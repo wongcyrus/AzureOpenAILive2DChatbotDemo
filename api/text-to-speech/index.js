1a65b7280eaa3ae16249245999c1f8ce4947b303
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2023-03-16 09:33:21 +0000

    try  context.done();

--- api/text-to-speech/index.js
+++ api/text-to-speech/index.js
@@ -11,41 +11,35 @@ const containerClient = blobServiceClient.getContainerClient("$web");
 module.exports = async function (context, req) {
     let body = req.body;
 
-    try {
-        const headers = {
-            'Content-Type': 'application/ssml+xml',
-            'X-Microsoft-OutputFormat': 'riff-8khz-16bit-mono-pcm',
-            'Ocp-Apim-Subscription-Key': ttsapikey,
-        }
-        const res = await axios.post(`https://${ttsregion}.tts.speech.microsoft.com/cognitiveservices/v1`, body, {
-            headers: headers
-        });
-        context.log("res");
-        context.log(res);
-        context.log("res.data");
-        context.log(typeof res.data);
-        context.log(res.data.length);
-        const data = await res.data;
-
-
-        context.log("after data");
-
-
-        const blobName = "newblob" + new Date().getTime() + ".wav";
-        const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-        const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
-        context.log(`Upload block blob ${blobName} successfully`, uploadBlobResponse.requestId);
-
-
-        context.res.json({
-            body: blobName
-        });
-
-
-    } catch (ex) {
-        context.log(ex);
-        context.res.json({
-            body: "" + ex
-        });
+    const headers = {
+        'Content-Type': 'application/ssml+xml',
+        'X-Microsoft-OutputFormat': 'riff-8khz-16bit-mono-pcm',
+        'Ocp-Apim-Subscription-Key': ttsapikey,
     }
+    const res = await axios.post(`https://${ttsregion}.tts.speech.microsoft.com/cognitiveservices/v1`, body, {
+        headers: headers
+    });
+    context.log("res");
+    context.log(res);
+    context.log("res.data");
+    context.log(typeof res.data);
+    context.log(res.data.length);
+    const data = await res.data;
+
+
+    context.log("after data");
+
+
+    const blobName = "newblob" + new Date().getTime() + ".wav";
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
+    context.log(`Upload block blob ${blobName} successfully`, uploadBlobResponse.requestId);
+
+
+    context.res = {
+        body: `Hello ${blobName}`
+    };
+    context.done();
+
+
 }
