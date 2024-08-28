8e23593c09a3858485e66edf87464dac545228fc
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2024-08-28 02:20:16 +0000

    Update CDN. Update Model.

--- api/constants.js
+++ api/constants.js
@@ -1,13 +1,14 @@
 const screenSharingPerMinute = 12;
 const screenSharingMaxSize = 0.5;
 const screenSharingMaxDuration = 3;
-const dailyCostLimit = 0.5;
+const dailyCostLimit = 0.3;
 
 const prices = {
-    'gpt-35-turbo': { "completion": { tokens: 1000, price: 0.0015 }, "prompt": { tokens: 1000, price: 0.002 } },
-    'gpt-35-turbo-16k': { "completion": { tokens: 1000, price: 0.004 }, "prompt": { tokens: 1000, price: 0.003 } },
-    'gpt-4-32k': { "completion": { tokens: 1000, price: 0.12 }, "prompt": { tokens: 1000, price: 0.06 } },
-    'gpt-4': { "completion": { tokens: 1000, price: 0.06 }, "prompt": { tokens: 1000, price: 0.03 } },
+    'gpt-35-turbo': { "completion": { tokens: 1000, price: 0.002 }, "prompt": { tokens: 1000, price: 0.002 } },
+    'gpt-35-turbo-16k': { "completion": { tokens: 1000, price: 0.0005 }, "prompt": { tokens: 1000, price: 0.002 } },
+    'gpt-4-32k': { "completion": { tokens: 1000, price: 0.06 }, "prompt": { tokens: 1000, price: 0.12 } },
+    'gpt-4': { "completion": { tokens: 1000, price: 0.03 }, "prompt": { tokens: 1000, price: 0.06 } },
+    'gpt-4o': { "completion": { tokens: 1000, price: 0.03 }, "prompt": { tokens: 1000, price: 0.06 } },
 }
 
 
